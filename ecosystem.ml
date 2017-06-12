26cc7f52d816ab1ce3e5f140d104821493752d08
Max de D <m.dinechin@yahoo.com>
2017-06-12 23:37:33 +0200

    first working version

--- ecosystem.ml
+++ ecosystem.ml
@@ -22,54 +22,13 @@ and instr = | P | M | L | R | W
 
 type polarity = Norm | Inv
 
+type mutation = Insert | Delete | Permut
+
 type joust_issue = Timeout | Capture | Exit
 
 type winner = Left | Tie | Right
 
 type individual = { x : int ; y : int ; fit : int ; life : int ; code : bot }
-
-
-
-(** TRACE **)
-
-let dark = rgb 39 40 34 ;;
-let lighter_gray = rgb 200 200 200
-
-
-let trace_init () =
-    Graphics.set_window_title "Ecosystem simulation!" ;
-    set_color dark ;
-    fill_rect 0 0 600 500 ;
-    moveto 174 380 ;c
-    set_color white ;
-    set_font "-*-fixed-medium-r-semicondensed--25-*-*-*-*-*-iso8859-1" ;
-    draw_string "Ecosystem Simulation" ;
-    moveto 8 430 ;
-    set_font "9x15" ;
-    set_color lighter_gray ;
-    draw_string "version 1.0" ;
-    let m = 8 in
-    moveto (50-m) (30-m) ;
-    lineto (50-m) (330+m) ;
-    lineto (550+m) (330+m) ;
-    lineto (550+m) (30-m) ;
-    lineto (50-m) (30-m)
-
-
-let clear () =
-    set_color dark ;
-    let m = 8 in
-    fill_rect (50-(m-1)) (30-(m-1)) (500+2*(m-1)) (300+2*(m-1))
-
-
-let trace_ind ind =
-    set_color lighter_gray ;
-    draw_circle (5 * ind.x + 50) (3 * ind.y + 30) (max 2 (ind.life / 50))
-
-
-let rec trace_pop = function
-    | t::q -> (trace_ind t ; trace_pop q)
-    | _ -> ()
 
 
 
@@ -134,7 +93,7 @@ let bot_Bigger_m       = ">->+>+>->------------------>++++++++++++++++++>-------
 
 
 (** Chooses against which bot the algorithm will learn to fight *)
-let objective_bot = bot_Bigger_m
+let objective_bot = bot_MickeyV4_m
 
 
 let score bot1 bot2 size pol =
@@ -162,7 +121,12 @@ let fitness bot =
     (score bot_s objective_bot 29 Inv )) / 10
 
 
-let life_expectancy bot = let x = (fitness bot + 20) in x*x
+let rec calc_fitness_pop = function
+    | t::q -> {x=t.x; y=t.y; fit=fitness t.code; life=t.life; code=t.code}::(calc_fitness_pop q)
+    | [] -> []
+
+
+let life_exp fit = let x = (fit + 22) in (x*x*x)/500
 
 
 let reduce_life ind =
@@ -193,16 +157,16 @@ let rec rand_bot len max_depth =
             | Lp _ -> if max_depth < 0 then aux i else
                       Lp (rand_bot (len/2) (max_depth-1)) :: (aux (i-1))
             | x -> x :: (aux (i-1))
-    in aux (1 + Random.int len)
+    in aux (1 + Random.int (max len 1))
 
 
 let rec rand_ind len max_depth =
     let bot = rand_bot len max_depth in
-    let fit = fitness bot in
-   {x = Random.int 99;
-    y = Random.int 99;
-    fit = fit;
-    life = (let x = (fit + 20) in x*x);
+    let fitn = fitness bot in
+   {x = Random.int 99 ;
+    y = Random.int 99 ;
+    fit = fitn ;
+    life = life_exp fitn ;
     code = bot}
 
 
@@ -212,14 +176,51 @@ let rec rand_pop size = match size with
 
 
 
-(* 3. MOVING *)
+
+(* 3. MUTATIONS *)
+
+let rand_mutation () = [|Insert ; Delete ; Permut|].(Random.int 3)
+
+
+let rec mutate_bot bot mut_prob =
+    let rec aux = function
+        | Lp l :: q -> Lp (mutate_bot l mut_prob) :: (aux q)
+        | t :: Lp l :: q -> t :: Lp (mutate_bot l mut_prob) :: (aux q)
+        | t :: t' :: q -> (
+            if rand () > mut_prob then t::(aux (t'::q))
+            else match rand_mutation () with
+                    | Insert -> if length bot > 80 then t::t'::q else
+                                t :: (rand_instr_std ()) :: (aux (t'::q))
+                    | Delete -> aux (t'::q)
+                    | Permut -> t'::t::(aux q)
+            )
+        | x -> x
+    in aux bot
+
+
+let mutate ind mut_prob =
+    let mutant = mutate_bot ind.code mut_prob in
+   {x = ind.x ; y = ind.y ;
+    fit = fitness mutant ;
+    life = ind.life ;
+    code = mutant}
+
+
+let rec mutate_pop pop mut_prob = match pop with
+    | t::q -> (if rand () < mut_prob then (mutate t mut_prob) else t)
+                :: (mutate_pop q mut_prob)
+    | [] -> []
+
+
+
+(* 4. MOVING *)
 
 let rd_int = Random.int
 
 
 let move ind =
-    { x = max 0 (min (ind.x - 3 + rd_int 7) 99) ;
-      y = max 0 (min (ind.y - 3 + rd_int 7) 99) ;
+    { x = max 0 (min (ind.x - 2 + rd_int 5) 99) ;
+      y = max 0 (min (ind.y - 2 + rd_int 5) 99) ;
       fit = ind.fit; life = ind.life ; code = ind.code}
 
 
@@ -229,7 +230,7 @@ let rec move_pop = function
 
 
 
-(* 4. BREEDING *)
+(* 5. BREEDING *)
 
 (** Outputs the first n elements from a list.
     If len(list) > n, outputs the whole list. *)
@@ -251,8 +252,8 @@ let rec last_n n list =
 let rec mate_bot bot1 bot2 =
     let l1, l2 = length bot1, length bot2 in
     if l2 >= l1 then
-        let p1 = Random.int l1 in
-        let p2 = Random.int (l2 - p1) in
+        let p1 = Random.int (max l1 1) in
+        let p2 = Random.int (max (l2 - p1) 1) in
         (first_n p1 bot1) @ (last_n p2 bot2)
     else
         mate_bot bot2 bot1
@@ -260,17 +261,16 @@ let rec mate_bot bot1 bot2 =
 
 let mate ind1 ind2 =
     let child = mate_bot ind1.code ind2.code in
-    let fit = fitness child in
-   {x = Random.int 99;
-    y = Random.int 99;
-    fit = fit;
-    life = (let x = (fit + 20) in x*x);
+    let fitn = fitness child in
+   {x = Random.int 99 ;
+    y = Random.int 99 ;
+    fit = fitn ;
+    life = life_exp fitn ;
     code = child}
 
 
 
-(* 6. ECOSYSTEM SIMULATION *)
-
+(* 6. POPULATION SORT *)
 
 (** Selects the n best individuals, removing doubles.
     If len(pop)<n, random individuals are added to replace the missing ones.
@@ -278,9 +278,8 @@ let mate ind1 ind2 =
     shorter is the better. *)
 let best_n n pop =
     let compare ind1 ind2 =
-        if ind1 = ind2 then 0 else
-            if ind2.fit = ind1.fit
-            then length ind1.code - length ind2.code
+        if ind2.fit = ind1.fit
+        then length ind1.code - length ind2.code
         else ind2.fit - ind1.fit
     in let sorted_pop = List.sort_uniq compare pop in
     if n <= List.length sorted_pop
@@ -297,36 +296,112 @@ let sort_pop pop =
     in List.sort compare pop
 
 
-let same_pos ind1 ind2 = (ind1.x = ind2.x) && (ind1.y = ind2.y)
+
+(** 7. TRACE **)
+
+let dark = rgb 39 40 34 ;;
+let grey = rgb 200 200 200 ;;
+let magenta = rgb 240 100 240 ;;
 
 
-(** Moves and Mates the population *)
-let next_generation pop =
+let trace_init () =
+    Graphics.set_window_title "Ecosystem simulation!" ;
+    set_color dark ;
+    fill_rect 0 0 600 500 ;
+    moveto 174 380 ;
+    set_color white ;
+    set_font "-*-fixed-medium-r-semicondensed--25-*-*-*-*-*-iso8859-1" ;
+    draw_string "Ecosystem Simulation" ;
+    moveto 8 430 ;
+    set_font "9x15" ;
+    set_color grey ;
+    draw_string "version 1.0" ;
+    for m = 11 to 15 do
+        moveto (50-m) (30-m) ;
+        lineto (50-m) (330+m) ;
+        lineto (550+m) (330+m) ;
+        lineto (550+m) (30-m) ;
+        lineto (50-m) (30-m)
+    done
+
+
+let clear () =
+    set_color dark ;
+    let m = 11 in
+    fill_rect (50-(m-1)) (30-(m-1)) (500+2*(m-1)) (300+2*(m-1))
+
+
+let trace_ind ind =
+    set_color white ;
+    draw_circle (5 * ind.x + 50) (3 * ind.y + 30) (max 1 (ind.life / 2))
+
+
+(* traces the five best individuals in red **)
+let rec trace_pop = function
+    | t::q -> (trace_ind t ; trace_pop q)
+    | _ -> ()
+
+
+(* 8. ECOSYSTEM SIMULATION *)
+
+let close_enough ind1 ind2 = (ind1.x = ind2.x)
+
+
+let ecosystem_gui nb_gen mut_prob =
+    let pop = ref (rand_pop 500) in
+    trace_init () ;
     let rec kill = function
         | t::q -> (if t.life > 0 then t::(kill q) else kill q)
         | [] -> []
     in let rec breed = function
-        | t::t'::q -> (if same_pos t t'
+        | t::t'::q -> (if close_enough t t'
                         then t :: (mate t t') :: (breed (t'::q))
                         else t :: (breed (t'::q)))
         | x -> x
-    in kill (breed (sort_pop (move_pop pop)))
-
-
-let ecosystem nb_gen =
-    let pop = ref (rand_pop 100) in
-    trace_init () ;
-    for i = 0 to nb_gen do
+    in for i = 0 to nb_gen do
+        pop := move_pop !pop ;
+        if (List.length !pop) > 50 then (pop := reduce_life_pop !pop) ;
+        pop := kill !pop ;
+        if (List.length !pop) < 100 then (pop := breed !pop) ;
+        pop := mutate_pop !pop mut_prob ;
+        if i mod 50 = 0 then begin
+            let best = hd (best_n 1 !pop) in
+            let fit, code = best.fit, (sob best.code) in
+            Printf.printf "Generation n°%d (f:%d) : %s" i fit code ;
+            print_newline ()
+        end ;
         clear () ;
         trace_pop !pop ;
-        pause 0.05 ;
-        pop := next_generation !pop ;
+        pause 0.02
     done ;
     let result = sob (hd (best_n 1 !pop)).code in
     Printf.printf "\nResult: %s\n\n" result ;
     result *>> objective_bot
 
 
-
-(* 6.1 - Standard ecosystem *)
-
+let ecosystem nb_gen mut_prob =
+    let pop = ref (rand_pop 500) in
+    let rec kill = function
+        | t::q -> (if t.life > 0 then t::(kill q) else kill q)
+        | [] -> []
+    in let rec breed = function
+        | t::t'::q -> (if close_enough t t'
+                        then t :: (mate t t') :: (breed (t'::q))
+                        else t :: (breed (t'::q)))
+        | x -> x
+    in for i = 0 to nb_gen do
+        pop := move_pop !pop ;
+        if (List.length !pop) > 50 then (pop := reduce_life_pop !pop) ;
+        pop := kill !pop ;
+        if (List.length !pop) < 100 then (pop := breed !pop) ;
+        pop := mutate_pop !pop mut_prob ;
+        if i mod 50 = 0 then begin
+            let best = hd (best_n 1 !pop) in
+            let fit, code = best.fit, (sob best.code) in
+            Printf.printf "Generation n°%d (f:%d) : %s" i fit code ;
+            print_newline ()
+        end ;
+    done ;
+    let result = sob (hd (best_n 1 !pop)).code in
+    Printf.printf "\nResult: %s\n\n" result ;
+    result *>> objective_bot
